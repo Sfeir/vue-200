4d204440022e563604c3556a8bb2bb652e961084
Cyril Balit <cbalit@gmail.com>
2017-05-23 18:31:56 +0200

    step-14-solution

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -6,49 +6,75 @@
         </md-card-header>
         <md-card-content>
             <img class="picture" :src="person.photo || 'https://randomuser.me/api/portraits/lego/6.jpg'" />            
-            <form @submit="submit">
-                <md-input-container :class="{'md-input-invalid': errors.has('firstname')}">
+            <form @submit="submit">                
+                <md-input-container :class="{ 'md-input-invalid': $v.person.firstname.$error }">
                     <label>Firstname</label>
-                    <md-input key="username-input" v-validate="'required|min:2'" v-model="person.firstname" data-vv-name="firstname"></md-input>
-                    <div v-show="errors.has('firstname')" class="md-error">{{ errors.first('firstname') }}</div>
+                    <md-input @input="$v.person.firstname.$touch()" v-model="person.firstname"></md-input>                
+                    <div class="md-error">
+                        <span v-show="!$v.person.firstname.required">Champs obligatoire</span>                         
+                        <span v-show="$v.person.firstname.required && !$v.person.firstname.maxLength">Vous devez saisir au moins 2 caractères</span>                         
+                    </div>                         
                 </md-input-container>
-                <md-input-container :class="{'md-input-invalid': errors.has('lastname')}">
+                <md-input-container :class="{ 'md-input-invalid': $v.person.lastname.$error }">
                     <label>Lastname</label>
-                    <md-input v-validate="'required|min:2'" data-vv-name="lastname" v-model="person.lastname"></md-input>
-                    <div v-show="errors.has('lastname')" class="md-error">{{ errors.first('lastname') }}</div>
+                    <md-input @input="$v.person.lastname.$touch()" v-model="person.lastname"></md-input>                
+                    <div class="md-error">
+                        <span v-show="!$v.person.lastname.required">Champs obligatoire</span>                         
+                        <span v-show="$v.person.lastname.required && !$v.person.lastname.maxLength">Vous devez saisir au moins 2 caractères</span>                         
+                    </div>                         
                 </md-input-container>
-                <md-input-container :class="{'md-input-invalid': errors.has('email')}">
+                <md-input-container :class="{ 'md-input-invalid': $v.person.email.$error }">
                     <label>Email</label>
-                    <md-input v-validate="'required|email'" data-vv-name="email" v-model="person.email"></md-input>
-                    <div v-show="errors.has('email')" class="md-error">{{ errors.first('email') }}</div>
+                    <md-input @input="$v.person.email.$touch()" v-model="person.email"></md-input>                
+                    <div class="md-error">
+                        <span v-show="$v.person.email.$error && !$v.person.email.required">Champs obligatoire</span>                         
+                        <span v-show="$v.person.email.$error && !$v.person.email.email">Vous devez saisir un mail valide</span>                                                 
+                    </div>                         
                 </md-input-container>
-                <md-input-container :class="{'md-input-invalid': errors.has('phone')}">
+                <md-input-container>
                     <label>Phone</label>
-                    <md-input v-validate="'required|regex:\d{10}'" data-vv-name="phone" v-model="person.phone"></md-input>
-                    <div v-show="errors.has('phone')" class="md-error">{{ errors.first('phone') }}</div>
+                    <md-input v-model="person.phone"></md-input>                                    
                 </md-input-container>
             </form>
         </md-card-content>
         <md-card-actions>
             <md-button @click.native.prevent="cancel">Cancel</md-button>
-            <md-button @click.native.prevent="submit" :disabled="errors.any()">Submit</md-button>
+            <md-button @click.native.prevent="submit":disabled="$v.person.$invalid">Submit</md-button>
         </md-card-actions>
-    </md-card>
-
+    </md-card>    
 </template>
 <script>
+    import { required, minLength, email } from 'vuelidate/lib/validators'
+    
+
     export default {
         props: {
-            person: {
-                type: Object,
-                default: function () {
-                    return {};
+            person:{
+                type:Object,
+                default:function(){
+                    return {firstname:'',lastname:'',email:'',phone:''};
                 }
             }
         },
         data() {
             return {
-
+             
+            }
+        },
+        validations: {
+            person:{
+                firstname: {
+                    required,
+                    minLength: minLength(2)
+                },
+                lastname: {
+                    required,
+                    minLength: minLength(2)
+                },
+                email: {
+                    required,
+                    email
+                }
             }
         },
         computed: {
