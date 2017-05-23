275a65a4f0e0b6bd928fd738ff9a1ce79fbd7a96
Cyril Balit <cbalit@gmail.com>
2017-05-23 18:36:11 +0200

    step-17

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <div class="header row">
+            <h1 class="col s12 flow-text">You have {{people.length}} contacts</h1>
+            <search-bar ></search-bar>
+        </div>
         <section class="container">
             <sfeir-card v-for="person in people" :person="person" @delete="deletePerson"></sfeir-card>
         </section>
@@ -20,12 +24,14 @@
     import CardPanel from '../components/CardPanel.vue'
     import Form from '../components/Form.vue'
     import peopleService from '../services/PeopleService.js';
+    import SearchBar from '../components/SearchBar.vue'
 
 
     export default {
         components: {
             'sfeir-card': CardPanel,
-            'sfeir-form': Form
+            'sfeir-form': Form,
+            'search-bar': SearchBar
         },
         data() {
             return {
@@ -36,7 +42,7 @@
             peopleService
                 .fetch()
                 .then(people => next(vm => {
-                    vm.people = people;
+                    vm._people = vm.people = people;
                 }))
                 .catch(console.log.bind(console))
         },
@@ -45,7 +51,7 @@
                 peopleService
                     .delete(person.id)
                     .then((people) => {
-                        this.people = people;
+                        this._people = this.people = people;
                     })
                     .catch(console.log)
             },
@@ -53,7 +59,7 @@
                 peopleService
                     .create(person)
                     .then((person) => {
-                        this.people.push(person);
+                        this._people.push(person);
                         this.hideDialog();
                     })
                     .catch(console.log)
@@ -66,6 +72,16 @@
                 this.$refs['dialog'].close();
                 this.showModal = false;
             },
+            filterPeople(search) {
+                if (!search) {
+                    this.people = this._people
+                }
+                else {
+                    this.people = this._people.filter(item => {
+                        return item.firstname.toLowerCase().indexOf(search) != -1 || item.lastname.toLowerCase().indexOf(search) != -1;
+                    });
+                }
+            }
         }
     }
 
@@ -79,86 +95,12 @@
         justify-content: center;
     }
     
-    .card-panel {
-        padding: 10px;
-        margin: 10px !important;
-        min-width: 500px;
-    }
-    
-    .card-panel:hover {
-        box-shadow: 0 11px 15px -7px rgba(0, 0, 0, .2), 0 24px 38px 3px rgba(0, 0, 0, .14), 0 9px 46px 8px rgba(0, 0, 0, .12);
-    }
-    
-    .md-card .md-subhead {
-        opacity: 1;
-    }
-    
-    .picture {
-        border-radius: 50%;
-        width: 100%;
-        height: 100%;
-    }
-    
-    .username {
-        color: #337ab7;
-        font-size: 24px;
-        font-weight: 400;
-    }
-    
-    .lastname {
-        text-transform: uppercase;
-    }
-    
-    .subtitle {
-        color: rgba(0, 0, 0, 0.54);
-    }
-    
-    .people-data {
-        margin-top: 20px;
-    }
-    
-    .item {
-        color: #337ab7;
-    }
-    
-    .people-data a {
-        padding-left: 10px;
-    }
-    
-    .icon {
-        color: lightslategrey;
-        width: 24px;
-        height: 24px;
-    }
-    
-    .label {
-        font-weight: bold;
-    }
-    
-    .skills {
-        padding: 10px;
-        margin: 10px;
-        background-color: #FAFAFA;
-    }
-    
-    .skills h3 {
-        font-size: 24px;
-        font-weight: normal;
-        line-height: 1.1;
-    }
-    
-    .skills a {
-        background-color: white;
-        color: #000;
-        margin: 5px;
-    }
-    
-    .links {
+    .header {
         text-align: center;
     }
-    
-    .links img {
-        padding: 0 5px;
+  
+    .header h1 {
+        font-weight: bold;
     }
 
 </style>
