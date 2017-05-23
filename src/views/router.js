158443d4c09cae251e0ac3afa2d6a756d1b644fc
Cyril Balit <cbalit@gmail.com>
2017-05-23 18:14:59 +0200

    step-08-solution

--- src/views/router.js
+++ src/views/router.js
@@ -1,12 +1,18 @@
 import VueRouter from 'vue-router'
 
 import Home from './Home.vue'
+import People from './People.vue'
 
 const routes = [
   {
     name: 'home',
     path: '/home',
     component: Home
+  },
+  {
+    name: 'people',
+    path: '/people',
+    component: People
   },
   {
     path: '/',
