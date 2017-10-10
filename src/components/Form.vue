1f35b6703a091d221a9b60b1fdd7fc887507a579
Cyril Balit <cbalit@gmail.com>
2017-10-10 12:46:11 +0200

    use form withou validation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -3,35 +3,30 @@
         <md-card-header v-if="editMode">
             <div class="md-title">Update {{ person.firstname }} {{ person.lastname }}</div>
             <div class="md-subhead">{{person.id}}</div>
-        </md-card-header>
+        </md-card-header>        
         <md-card-content>
-            <img class="picture" :src="person.photo || 'https://randomuser.me/api/portraits/lego/6.jpg'" />            
-            <form @submit="submit">
-                <md-input-container :class="{'md-input-invalid': errors.has('firstname')}">
-                    <label>Firstname</label>
-                    <md-input key="username-input" v-validate="'required|min:2'" v-model="person.firstname" data-vv-name="firstname"></md-input>
-                    <div v-show="errors.has('firstname')" class="md-error">{{ errors.first('firstname') }}</div>
-                </md-input-container>
-                <md-input-container :class="{'md-input-invalid': errors.has('lastname')}">
-                    <label>Lastname</label>
-                    <md-input v-validate="'required|min:2'" data-vv-name="lastname" v-model="person.lastname"></md-input>
-                    <div v-show="errors.has('lastname')" class="md-error">{{ errors.first('lastname') }}</div>
-                </md-input-container>
-                <md-input-container :class="{'md-input-invalid': errors.has('email')}">
-                    <label>Email</label>
-                    <md-input v-validate="'required|email'" data-vv-name="email" v-model="person.email"></md-input>
-                    <div v-show="errors.has('email')" class="md-error">{{ errors.first('email') }}</div>
-                </md-input-container>
-                <md-input-container :class="{'md-input-invalid': errors.has('phone')}">
-                    <label>Phone</label>
-                    <md-input v-validate="'required|regex:\d{10}'" data-vv-name="phone" v-model="person.phone"></md-input>
-                    <div v-show="errors.has('phone')" class="md-error">{{ errors.first('phone') }}</div>
-                </md-input-container>
+            <img class="picture" :src="person.photo || 'https://randomuser.me/api/portraits/lego/6.jpg'" />
+            <md-input-container>
+                <label>Firstname</label>
+                <md-input name="firstname" v-model="person.firstname"></md-input>
+            </md-input-container>
+            <md-input-container>
+                <label>Lastname</label>
+                <md-input name="lastname" v-model="person.lastname"></md-input>
+            </md-input-container>
+            <md-input-container>
+                <label>Email</label>
+                <md-input name="email" v-model="person.email"></md-input>
+            </md-input-container>
+            <md-input-container>
+                <label>Phone</label>
+                <md-input name="phone" v-model="person.phone"></md-input>
+            </md-input-container>
             </form>
         </md-card-content>
         <md-card-actions>
-            <md-button @click.native.prevent="cancel">Cancel</md-button>
-            <md-button @click.native.prevent="submit" :disabled="errors.any()">Submit</md-button>
+            <md-button @click.native="cancel">Cancel</md-button>
+            <md-button @click.native="submit">Submit</md-button>
         </md-card-actions>
     </md-card>
 
@@ -39,16 +34,16 @@
 <script>
     export default {
         props: {
-            person: {
-                type: Object,
-                default: function () {
-                    return {};
+            person:{
+                type:Object,
+                default:function(){
+                    return {firstname:'',lastname:'',email:'',phone:''};
                 }
             }
         },
         data() {
             return {
-
+                
             }
         },
         computed: {
@@ -65,7 +60,6 @@
             }
         }
     }
-
 </script>
 <style scoped>
     .card-panel {
